002f5321ccf79b184fd6f13cd9b5cdb596c60c7d
Marika <mballely@gmail.com>
2016-02-19 08:39:12 -0800

    test passing for multiple instances

--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -14,5 +14,17 @@
             // Assert
             $this->assertEquals(1, $result);
         }
+
+        function test_CountRepeats_Multiple()
+        {
+            // Arrange
+            $test_Count = new RepeatCounter;
+            $input = "the";
+            $string = "So the man ate the sandwich";
+            // Act
+            $result = $test_Count->CountRepeats($input, $string);
+            // Assert
+            $this->assertEquals(2, $result);
+        }
     }
  ?>
