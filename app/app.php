21a33536f6b49ff5b495067351752149df22b465
Marika <mballely@gmail.com>
2016-02-19 09:27:20 -0800

    add twig pages

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -8,7 +9,17 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
-            // twig input associative array
+            'form' => true
+        ));
+    });
+
+    $app->get("/result", function() use ($app) {
+        $new_counter = new RepeatCounter;
+        $result = $new_counter->CountRepeats($_GET['input'], $_GET['string']);
+        return $app['twig']->render('index.html.twig', array(
+            'result' => array(
+                'text' => $result
+            )
         ));
     });
 
